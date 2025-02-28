0ce5732c10fe0079e3f5809c26af9f8b95865d06
Hoang Phi <69hoangphi69@gmail.com>
2025-02-28 19:16:57 +0700

    init

---
+++ src/test/java/F01.java
@@ -0,0 +1,99 @@
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class F01 {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeMethod
+    public void setUp() {
+      // System.setProperty("webdriver.chrome.driver", "C:\\Tester\\chromedriver-win64\\chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("https://phptravels.net/#");
+    }
+
+    public void searchHotel(String location, String checkinDate, String checkoutDate, int guests) {
+        if (!location.isEmpty()) { // Nếu location không rỗng, nhập dữ liệu vào ô tìm kiếm
+            WebElement searchBox = wait.until(ExpectedConditions.elementToBeClickable(By.id("select2-hotels_city-container")));
+            searchBox.click();
+            WebElement searchInput = driver.findElement(By.xpath("//input[@class='select2-search__field']"));
+            searchInput.sendKeys(location);
+            WebElement resall = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//li[contains(@class, 'select2-results__option')]")));
+            resall.click();
+        }
+
+        // Nhập ngày nhận/trả phòng
+        driver.findElement(By.id("checkin")).clear();
+        driver.findElement(By.id("checkin")).sendKeys(checkinDate);
+        driver.findElement(By.id("checkout")).clear();
+        driver.findElement(By.id("checkout")).sendKeys(checkoutDate);
+
+        // Nhập số khách
+        WebElement guestBox = driver.findElement(By.id("travellersInput"));
+        guestBox.click();
+        WebElement adult = driver.findElement(By.id("adultInput"));
+        adult.clear();
+        adult.sendKeys(String.valueOf(guests));
+
+        driver.findElement(By.id("hotels-search")).click();
+    }
+
+    @Test
+    public void testValidHotel() {
+        searchHotel("Dubai", "2025-03-01", "2025-03-07", 2);
+        WebElement resultTitle = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h2[contains(text(), 'Hotels in')]")));
+        Assert.assertTrue(resultTitle.getText().contains("Hotels in"), "Kết quả tìm kiếm không đúng!");
+    }
+
+    @Test
+    public void testSearchWithoutLocation() {
+        searchHotel("", "2025-03-01", "2025-03-07", 2);
+        WebElement error = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class, 'error-message')]")));
+        Assert.assertTrue(error.isDisplayed(), "Không có thông báo lỗi khi thiếu địa điểm!");
+    }
+
+    @Test
+    public void testCheckinBeforeCheckout() {
+        searchHotel("Dubai", "2025-03-07", "2025-03-01", 2);
+        WebElement error = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(), 'Check-out date must be after Check-in date')]")));
+        Assert.assertTrue(error.isDisplayed(), "Không có thông báo lỗi khi ngày checkout trước checkin!");
+    }
+
+    @Test
+    public void testExceedGuestLimit() {
+        searchHotel("Dubai", "2025-03-01", "2025-03-07", 100);
+        WebElement error = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(), 'Maximum guest limit exceeded')]")));
+        Assert.assertTrue(error.isDisplayed(), "Không có thông báo lỗi khi số lượng khách vượt quá giới hạn!");
+    }
+
+    @Test
+    public void testPastCheckinDate() {
+        searchHotel("Dubai", "2024-02-22", "2025-03-07", 2);
+        WebElement error = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(), 'Check-in date cannot be in the past')]")));
+        Assert.assertTrue(error.isDisplayed(), "Không có thông báo lỗi khi nhập ngày nhận phòng là ngày quá khứ!");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
---
+++ src/test/java/F02.java
@@ -0,0 +1,178 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class F02 {
+  public WebDriver driver;
+  public String url = "http://phptravels.net";
+  
+  @BeforeMethod
+  public void launchBrowser() throws InterruptedException { 
+    WebDriverManager.firefoxdriver().setup();
+    driver = new FirefoxDriver();
+    driver.get(url);
+    driver.manage().window().maximize();
+    Thread.sleep(500);
+  }
+    
+  @Test
+  public void TC01_ValidSearch() throws InterruptedException {
+    performFlightSearch(
+      "oneway",
+      "economy_premium",
+      "HAN - Noi Bai International Airport",
+      "NYC - All Airports",
+      5,
+      "2",
+      true
+    );
+  }
+  
+  @Test
+  public void TC02_SearchWithoutDeparture() throws InterruptedException {
+    performFlightSearch(
+      "oneway",
+      "economy",
+      "",
+      "",
+      0,
+      "0",
+      false,
+      "Departure City cannot be empty"
+    );
+  }
+
+  @Test
+  public void TC03_SearchWithoutDestination() throws InterruptedException {
+    performFlightSearch(
+      "oneway", 
+      "economy", 
+      "HAN - Noi Bai International Airport", 
+      "", 
+      5, 
+      "1", 
+      false, 
+      "Destination City cannot be empty"
+    );
+  }
+
+  @Test
+  public void TC04_SearchWithPastDate() throws InterruptedException {
+    performFlightSearch(
+      "oneway", 
+      "business", 
+      "SGN - Tan Son Nhat International Airport", 
+      "LAX - Los Angeles International Airport", 
+      -2, 
+      "1", 
+      false, 
+      "Invalid departure date"
+    );
+  }
+
+  @Test
+  public void TC05_SearchWithInvalidPassengerNumber() throws InterruptedException {
+    performFlightSearch(
+      "oneway", 
+      "first", 
+      "DAD - Da Nang International Airport", 
+      "CDG - Charles de Gaulle International Airport", 
+      10,
+      "-1", 
+      false, 
+      "Invalid passenger number"
+    );
+  }
+  
+  public void performFlightSearch(String flightType, String ticketClass, String from, String to, int daysFromToday,
+      String passengers, boolean expectSuccess) throws InterruptedException {
+    performFlightSearch(flightType, ticketClass, from, to, daysFromToday, passengers, expectSuccess, "");
+  }
+
+  public void performFlightSearch(String flightType, String ticketClass, String from, String to, int daysFromToday, String passengers, boolean expectSuccess, String alertMessage) throws InterruptedException {
+    // Chọn tab chuyến bay
+    WebElement flightTabElement = driver.findElement(By.xpath("/html/body/main/div[1]/div[2]/div[2]/div/div/ul/li[1]/button"));
+    flightTabElement.click();
+
+    Thread.sleep(500);
+
+    // Chọn loại chuyến bay
+    WebElement flightWayElement = driver.findElement(By.xpath("/html/body/main/div[1]/div[2]/div[2]/div/div/div/div/div[1]/form/div[1]/div/div/div[1]/select"));
+    Select flightWay = new Select(flightWayElement);
+    flightWay.selectByValue(flightType);
+
+    // Chọn hạng vé
+    Thread.sleep(500);
+    WebElement flightClassElement = driver.findElement(By.xpath("//*[@id=\"flight_type\"]"));
+    Select flightClass = new Select(flightClassElement);
+    flightClass.selectByValue(ticketClass);
+
+    // Chọn điểm xuất phát
+    Thread.sleep(500);
+    WebElement fromElement = driver.findElement(By.xpath("/html/body/main/div[1]/div[2]/div[2]/div/div/div/div/div[1]/form/div[2]/div[1]/div/input"));
+    fromElement.sendKeys(from);
+
+    // Chọn điểm đến
+    Thread.sleep(500);
+    WebElement toElement = driver.findElement(By.xpath("/html/body/main/div[1]/div[2]/div[2]/div/div/div/div/div[1]/form/div[2]/div[2]/div[2]/input"));
+    toElement.sendKeys(to);
+
+    // Chọn ngày xuất phát
+    Thread.sleep(500);
+    LocalDate futureDate = LocalDate.now().plusDays(daysFromToday);
+    String formattedDate = futureDate.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+
+    WebElement dateInput = driver.findElement(By.xpath("//*[@id=\"departure\"]"));
+    dateInput.clear();
+    dateInput.sendKeys(formattedDate);
+
+    // Chọn số lượng hành khách
+    Thread.sleep(500);
+    WebElement travellersElement = driver.findElement(By.xpath("/html/body/main/div[1]/div[2]/div[2]/div/div/div/div/div[1]/form/div[2]/div[4]/div/div/div/a"));
+    travellersElement.click();
+
+    WebElement fAdultsElement = driver.findElement(By.xpath("//*[@id=\"fadults\"]"));
+    fAdultsElement.clear();
+    fAdultsElement.sendKeys(passengers);
+
+    // Bấm nút tìm kiếm chuyến bay
+    Thread.sleep(500);
+    WebElement flightSearchElement = driver.findElement(By.xpath("//*[@id=\"flights-search\"]"));
+    flightSearchElement.submit();
+
+    
+    // Kiểm tra tiêu đề trang kết quả hoặc thông báo lỗi
+    if (expectSuccess) {
+      Thread.sleep(1000);
+      String actualTitle = driver.getTitle();
+      String expectedTitle = "Flights Result";
+      Assert.assertEquals(actualTitle, expectedTitle, "Trang dẫn có tiêu đề không đúng");
+    } else {
+      Thread.sleep(500);
+      Alert alert = driver.switchTo().alert();
+      String actualMessage = alert.getText();
+      String expectedMessage = alertMessage;
+      Assert.assertEquals(actualMessage, expectedMessage, "Nội dung alert sai");
+      alert.accept();
+    }
+
+    Thread.sleep(3000);
+  }
+
+  @AfterMethod
+  public void terminateBrowser() {
+    driver.close();
+  }
+}
