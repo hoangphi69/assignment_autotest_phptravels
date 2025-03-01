29961642ffe34ec21293764e611d817e0e6b9b81
Hoang Phi <69hoangphi69@gmail.com>
2025-03-01 18:34:31 +0700

    added test cases

--- src/test/java/Test_Script/F03.java
+++ src/test/java/Test_Script/F03.java
@@ -2,89 +2,131 @@ package Test_Script;
 
 import java.time.Duration;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import utils.BaseTest;
 
-public class F03 extends BaseTest{
-    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+public class F03 extends BaseTest {
+  WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
-    // TC01: nhập đúng trường thông tin
-    @Test
-    public void TC01_Correct() throws InterruptedException {
-        perform_testTours("Tokyo");
-        
+  // TC01: nhập đúng trường thông tin
+  @Test
+  public void TC01_Correct() {
+    perform_testTours("Tokyo", "25-04-2025", "1");
+  }
+
+  // TC02: Nhập sai địa điểm (tham số location lại trả về kết quả trước đó khi
+  // nhập sai tên)
+  @Test
+  public void TC02_IncorrectLocation() {
+    perform_testTours("GitGud", "25-04-2025", "1");
+
+    // Kiểm tra thông báo từ input
+    JavascriptExecutor js = (JavascriptExecutor) driver;
+    WebElement location = driver.findElement(By.xpath("//*[@id=\"tours_city\"]"));
+    String actual = (String) js.executeScript("return arguments[0].validationMessage;", location);
+    String expected = "Please select an item in the list.";
+    Assert.assertEquals(actual, expected, "Thông báo sai");
+  }
+
+  // TC03: Không nhập địa điểm (tham số location lại trả về kết quả trước đó khi
+  // không nhập địa điểm)
+  @Test
+  public void TC03_LocationBlank() {
+    perform_testTours("", "25-04-2025", "1");
+
+    // Kiểm tra thông báo từ input
+    JavascriptExecutor js = (JavascriptExecutor) driver;
+    WebElement location = driver.findElement(By.xpath("//*[@id=\"tours_city\"]"));
+    String actual = (String) js.executeScript("return arguments[0].validationMessage;", location);
+    String expected = "Please select an item in the list.";
+    Assert.assertEquals(actual, expected, "Thông báo sai");
+  }
+
+  // TC04: Nhập thời gian quá khứ
+  @Test
+  public void TC04_DateInThePast() {
+    perform_testTours("Tokyo", "25-04-2024", "1");
+
+    // Kiểm tra thông báo từ alert
+    Alert alert = driver.switchTo().alert();
+    String actual = alert.getText();
+    String expected = "past";
+    Assert.assertTrue(actual.contains(expected), "Thông báo sai: " + actual);
+  }
+
+  // TC05: Nhập số lượng hành khách là '0'
+  @Test
+  public void TC05_ZeroTraveller() {
+    perform_testTours("Tokyo", "25-04-2025", "0");
+
+    // Kiểm tra thông báo từ alert
+    Alert alert = driver.switchTo().alert();
+    String actual = alert.getText();
+    String expected = "past";
+    Assert.assertTrue(actual.contains(expected), "Thông báo sai: " + actual);
+  }
+
+  public void perform_testTours(String location, String depart_date, String adults_number) {
+    JavascriptExecutor js = (JavascriptExecutor) driver;
+
+    // Chọn Tours
+    WebElement tours = driver.findElement(By.xpath("/html/body/main/div[1]/div[2]/div[2]/div/div/ul/li[3]/button"));
+    tours.click();
+
+    // Chọn địa điểm Tours
+    WebElement locationField = driver.findElement(By.xpath("//*[@id=\"select2-tours_city-container\"]"));
+    locationField.click();
+    delay(500);
+
+    if (!location.isEmpty()) { // Nhập input
+      // Nhập địa điểm Tours
+      WebElement locationInput = driver.findElement(By.xpath("/html/body/span/span/span[1]/input"));
+      locationInput.sendKeys(location);
+      delay(500);
+
+      // Nhấn Enter
+      locationInput.sendKeys(Keys.ENTER);
+      delay(500);
+    } else { // Không nhập input
+      System.out.println("Thông báo lỗi TC03: Vui lòng chọn địa điểm");
     }
 
-    // TC02: Nhập sai địa điểm (tham số location lại trả về kết quả trước đó khi nhập sai tên)
-    @Test
-    public void TC02_IncorrectLocation() throws InterruptedException {
-        perform_testTours("GitGud");
-        WebElement noResult = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/span/span/span[2]/ul/li")));
-        String noResultMessage = noResult.getText();
-        System.out.println("Thông báo lỗi TC02:" + noResultMessage);
-    }
+    // // Chọn thời gian đi
+    WebElement departDate = driver.findElement(By.xpath("//*[@id=\"date\"]"));
+    js.executeScript("arguments[0].removeAttribute('readonly');", departDate);
+    departDate.clear();
+    departDate.sendKeys(depart_date);
+    delay(500);
 
-    // TC03: Không nhập địa điểm (tham số location lại trả về kết quả trước đó khi không nhập địa điểm)
-    @Test
-    public void TC03_LocationBlank() throws InterruptedException {
-        perform_testTours("");
-    }
+    // Chọn số lượng khách
+    WebElement travellers = driver.findElement(
+        By.xpath("/html/body/main/div[1]/div[2]/div[2]/div/div/div/div/div[3]/form/div/div[3]/div/div/div/a"));
+    travellers.click();
 
-    
+    WebElement adults = driver.findElement(By.xpath("//*[@id=\"tours_adults\"]"));
+    adults.clear();
+    adults.sendKeys(adults_number);
+    delay(500);
 
-    public void perform_testTours(String location) throws InterruptedException {
+    // Nhấn tìm kiếm
+    WebElement findBtn = driver.findElement(
+        By.xpath("/html/body/main/div[1]/div[2]/div[2]/div/div/div/div/div[3]/form/div/div[4]/button"));
+    findBtn.click();
+    delay(5000);
+  }
 
-        // Chọn Tours
-        WebElement tours = driver.findElement(By.xpath("/html/body/main/div[1]/div[2]/div[2]/div/div/ul/li[3]/button"));
-        tours.click();
-        
-        // Chọn địa điểm Tours
-        WebElement locationField = driver.findElement(By.xpath("//*[@id=\"select2-tours_city-container\"]"));
-        locationField.click();
-        delay(500);
-
-        
-        if (!location.isEmpty()) { // Nhập input 
-
-            // Nhập địa điểm Tours
-            WebElement locationInput = driver.findElement(By.xpath("/html/body/span/span/span[1]/input"));
-            locationInput.sendKeys(location);
-            delay(500);
-
-            // Nhấn Enter
-            locationInput.sendKeys(Keys.ENTER);
-            delay(500);
-        } else { // Không nhập input
-            System.out.println("Thông báo lỗi TC03: Vui lòng chọn địa điểm");
-        }
-
-        // Chọn số lượng khách
-        WebElement guestBox = driver.findElement(By.xpath("/html/body/main/div[1]/div[2]/div[2]/div/div/div/div/div[3]/form/div/div[3]/div/div/div/a"));
-        guestBox.click();
-        WebElement adultsAdd = driver.findElement(By.xpath("/html/body/main/div[1]/div[2]/div[2]/div/div/div/div/div[3]/form/div/div[3]/div/div/div/div/div[1]/div/div/div[2]"));
-        adultsAdd.click();
-
-        // // Chọn thời gian đi
-        // WebElement departDateDropDown = driver.findElement(By.xpath("//*[@id=\"date\"]"));
-        // departDateDropDown.click();
-        // // >>>ERROR<<<
-        // WebElement departDateInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[8]")));
-        // WebElement departDateSelect = departDateInput.findElement(By.xpath("/html/body/div[8]/div[1]/table/tbody/tr[3]/td[4]"));
-        // departDateSelect.click();
-
-        // Nhấn tìm kiếm
-        WebElement findBtn = driver.findElement(By.xpath("/html/body/main/div[1]/div[2]/div[2]/div/div/div/div/div[3]/form/div/div[4]/button"));
-        findBtn.click();
-        delay(1000);
-
-        driver.navigate().back();
-        delay(1000);
-
-    }
+  @AfterMethod
+  public void navigateBack() {
+    driver.get(url);
+    delay(500);
+  }
 }
