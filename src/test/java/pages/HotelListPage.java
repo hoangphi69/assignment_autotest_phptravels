51070c861a2b550a389f160b3d1fd7a338a84d3e
Hoang Phi <69hoangphi69@gmail.com>
2025-03-28 13:06:22 +0700

    added hotel search function tests

--- src/test/java/pages/HotelListPage.java
+++ src/test/java/pages/HotelListPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -14,6 +15,13 @@ public class HotelListPage {
   private JavascriptExecutor js;
   private WebDriverWait wait;
 
+  // Component danh sách khách sạn
+  public By HOTEL_LIST = By.cssSelector("ul.HotelsData.list-unstyled");
+
+  public By HOTEL_ITEM = By.cssSelector("li.card--item");
+
+  public By HOTEL_NAME = By.cssSelector("h5");
+
   // Component tìm kiếm danh sách theo tên
   public By SEARCH_NAME_INPUT = By.xpath("/html/body/main/section/div[2]/div/div[1]/div/div[1]/div[2]/div/input");
 
@@ -22,6 +30,27 @@ public class HotelListPage {
     this.driver = driver;
     js = (JavascriptExecutor) driver;
     wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+  }
+
+  // Delay page
+  public void delay(long milliseconds) {
+    try {
+      Thread.sleep(milliseconds);
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+    }
+  }
+
+  // Lấy danh sách các khách sạn
+  public List<WebElement> getHotels() {
+    WebElement list = driver.findElement(HOTEL_LIST);
+    List<WebElement> items = list.findElements(HOTEL_ITEM);
+    return items;
+  }
+
+  // Lấy tên khách sạn
+  public String getHotelName(WebElement hotel) {
+    return hotel.findElement(HOTEL_NAME).getText();
   }
 
   // Thực hiện tìm kiếm khách sạn theo tên
